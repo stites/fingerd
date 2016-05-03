bed1e2c1d0e6673eb281d3f3c4bd4c398099bcd8
stites <sam@stites.io>
2016-05-02 21:04:54 -0400

    add log and echo

---
+++ src/Debug.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import Control.Monad (forever)
+import Network.Socket hiding (recv)
+import Network.Socket.ByteString hiding (recv, sendAll)
+
+logAndEcho :: Socket -> IO ()
+logAndEcho sock = forever $ do     -- ^ loop this forever with the y-combinator
+  (soc, _) <- accept sock          -- ^ block until a connection to the server is made
+  printAndKickback soc             -- ^ do stuff
+  sClose soc                       -- ^ close our connection
+  where printAndKickback conn = do
+          msg <- recv conn 1024    -- ^ get up to 1024 bytes of text from the connection
+          print msg                -- ^ print it literally
+          sendAll conn msg         -- ^ relay the message back to the connection
+
